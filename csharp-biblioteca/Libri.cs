19f5693b7e5ea4fa1533a87d5feabb135447b079
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-11 15:05:38 +0200

    Crezione della classe libro che estende la classe documento

--- csharp-biblioteca/Libri.cs
+++ csharp-biblioteca/Libri.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace csharp_biblioteca
 {
-    public class Libri
+    public class Libri : Documento
     {
+        public int Pagine { get; set; }
+
+        public Libri(string titolo, int anno, string settore, char scaffale, string autore, int pagine) : base(titolo, anno, settore, scaffale, autore)
+        {
+            this.Pagine = pagine;
+        }
+
     }
 }
