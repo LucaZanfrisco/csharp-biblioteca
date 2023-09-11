fb2da5302de3ad386e5c7e02d165e24ecf3d2fc8
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-11 15:20:25 +0200

    Creazione della classe dvd

---
+++ csharp-biblioteca/Dvd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Dvd : Documento
+    {
+        public int Durata {  get; set; }
+        public Dvd(string titolo, int anno, string settore, char scaffale, string autore, int durata) : base(titolo, anno, settore, scaffale, autore)
+        {
+            if(durata > 0)
+            {
+                this.Durata = durata;
+            }
+        }
+    }
+}
